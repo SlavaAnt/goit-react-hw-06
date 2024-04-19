6a604df5b1de8716ca57929a6f7df76b7c292b29
Slava Antolyk <Sl.antolyk@gmail.com>
2024-04-19 19:22:52 +0300

    fix searching by number

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -6,14 +6,12 @@ import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 
 const getFilterContacts = (contacts, filter) => {
+  console.log(contacts);
   if (filter.length > 0) {
-    return contacts.filter(({ name, number }) =>
-      name
-        .toLowerCase()
-        .includes(
-          filter.trim().toLowerCase() ||
-            number.toLowerCase().includes(filter.trim().toLowerCase())
-        )
+    return contacts.filter(
+      ({ name, number }) =>
+        name.toLowerCase().includes(filter.trim().toLowerCase()) ||
+        number.includes(filter.trim())
     );
   } else {
     return contacts;
