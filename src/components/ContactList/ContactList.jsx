06f5b16a8c32febb96e200bbb6ae122ec561fde4
Slava Antolyk <Sl.antolyk@gmail.com>
2024-04-19 19:12:16 +0300

    add redux and redux toolkit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,45 @@
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+
+const getFilterContacts = (contacts, filter) => {
+  if (filter.length > 0) {
+    return contacts.filter(({ name, number }) =>
+      name
+        .toLowerCase()
+        .includes(
+          filter.trim().toLowerCase() ||
+            number.toLowerCase().includes(filter.trim().toLowerCase())
+        )
+    );
+  } else {
+    return contacts;
+  }
+};
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+  const filterContacts = getFilterContacts(contacts, filter);
+
+  return (
+    <ul className={css.contactsContainer}>
+      {filterContacts.map((contact) => {
+        return (
+          <li key={contact.id} className={css.contactBox}>
+            <Contact
+              name={contact.name}
+              number={contact.number}
+              id={contact.id}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
